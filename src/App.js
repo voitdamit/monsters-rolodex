969d0a2f600f6beda7d8746ceb9eaa27277f10df
Will Ripka <voitdamit@gmail.com>
2023-06-07 11:41:45 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import { Component } from 'react';
+
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: 'Willy'
+    };
+  }
+
+
+  render() {
+     return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Hi {this.state.name}!
+            </p>
+            <button>Change Name</button>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
